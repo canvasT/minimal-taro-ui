52f210a2969151347e825bdd57629a9b185b6036
Taotao Feng <fengtaotao082913@gmail.com>
2019-06-30 10:48:09 +0800

    add: 增加mainpackage

--- src/components/checkbox/index.tsx
+++ src/components/checkbox/index.tsx
@@ -1,6 +1,7 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
 import "@minimal-taro-ui/base-style"
+import classnames from 'classnames'
 
 interface IProps {
   title: string;
